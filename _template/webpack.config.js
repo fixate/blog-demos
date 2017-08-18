9ad1734cb860b533477a98aa0ef7c795d7f518fe
Larry Botha <larry@fixate.it>
2017-08-18 14:06:56 +0200

    remove preact from template

--- _template/webpack.config.js
+++ _template/webpack.config.js
@@ -7,11 +7,4 @@ module.exports = Object.assign({}, webpackConf, {
   entry: {
     main: './index.js',
   },
-
-  resolve: Object.assign({}, webpackConf.resolve, {
-      alias: {
-        react: 'preact-compat',
-        'react-dom': 'preact-compat',
-      },
-  }),
 });
